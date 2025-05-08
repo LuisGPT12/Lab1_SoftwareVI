f0269a65a5b2b3c48600c638dc9ff9fe7a5c34eb
LuisGPT12 <monterrosaluis429@gmail.com>
2025-05-08 16:10:07 -0500

    commit-layout

--- app/src/main/java/com/example/lab1_softwarevi/MainActivity.kt
+++ app/src/main/java/com/example/lab1_softwarevi/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lab1_softwarevi
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -14,15 +16,14 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(R.layout.main_activity)
-
       val toolbar = findViewById<Toolbar>(R.id.toolbar)
+      val text = findViewById<TextView>(R.id.textView)
       setSupportActionBar(toolbar)
-
       val button = findViewById<Button>(R.id.button)
       button.setOnClickListener {
+          text.visibility= View.GONE
           supportFragmentManager.beginTransaction().apply {
               replace(R.id.fragment_container, Fragment_1())
-              addToBackStack(null)
               commit()
           }
       }
