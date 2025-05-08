8492044c0f065d703456dd03d964c419f3fd659c
LuisGTP12 <monterrosaluis@gmail.com>
2025-05-07 21:48:01 -0500

    Commit-un commit

--- app/src/main/java/com/example/lab1_softwarevi/MainActivity.kt
+++ app/src/main/java/com/example/lab1_softwarevi/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.lab1_softwarevi
-
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
+/**
+ * Justin Barrios, Cédula: 8-983-1021
+ * Luis Monterrosa, Cédula: 8-1014-1095
+ * Aaron Santamaria, Cédula: 3-742-1763
+ **/
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.main_activity)
 
-    }
+      val toolbar = findViewById<Toolbar>(R.id.toolbar)
+      setSupportActionBar(toolbar)
 
-
+      val button = findViewById<Button>(R.id.button)
+      button.setOnClickListener {
+          supportFragmentManager.beginTransaction().apply {
+              replace(R.id.fragment_container, Fragment_1())
+              addToBackStack(null)
+              commit()
+          }
+      }
+  }
 }
 
