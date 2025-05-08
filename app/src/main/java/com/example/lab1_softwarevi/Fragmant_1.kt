7612aa09e2275de28902b6681c1becb9b66e9893
ZappyBugin <jfbarrios16@gmail.com>
2025-05-07 20:06:51 -0400

    commit create of the fragment for the dice funtion

---
+++ app/src/main/java/com/example/lab1_softwarevi/Fragmant_1.kt
@@ -0,0 +1,14 @@
+package com.example.lab1_softwarevi
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Fragment : Fragment_1() {
+
+}
+
+open class Fragment_1 {
+
+}
