5ca129eaab6f17a2eacd02e1afc3b5a5833ab0c7
AaronSant99 <aaron.santamaria1@utp.ac.pa>
2025-05-07 19:11:23 -0500

    Implementacion de las librerias Konfetti y Material Components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.material)
+    implementation(libs.konfetti)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
