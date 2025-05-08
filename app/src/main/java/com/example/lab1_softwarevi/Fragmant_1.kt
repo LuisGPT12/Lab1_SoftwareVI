35d2ceb608d954d432c22de99c92d7d1cd47565a
ZappyBugin <jfbarrios16@gmail.com>
2025-05-07 20:53:41 -0400

    commit update the fragment_1 for the uso of dice

--- app/src/main/java/com/example/lab1_softwarevi/Fragmant_1.kt
+++ app/src/main/java/com/example/lab1_softwarevi/Fragmant_1.kt
@@ -1,14 +1,39 @@
 package com.example.lab1_softwarevi
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.VideoView
+import androidx.fragment.app.Fragment
 
-class Fragment : Fragment_1() {
+class Fragment_1 : Fragment() {
 
-}
+    private lateinit var videodado: VideoView
 
-open class Fragment_1 {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_1, container, false)
+        videodado = view.findViewById(R.id.dadoVideo)
+        randomDice()
+        return view
+    }
 
+    private fun randomDice() {
+        val randomNumber = (1..6).random()
+        val videoName = "dado$randomNumber"
+        val videoResId = resources.getIdentifier(videoName, "raw", requireContext().packageName)
+        val uri = Uri.parse("android.resource://${requireContext().packageName}/$videoResId")
+        videodado.setVideoURI(uri)
+        videodado.setOnPreparedListener { mediaPlayer ->
+            mediaPlayer.isLooping = false
+        }
+        videodado.setOnCompletionListener {
+            Toast.makeText(requireContext(), "Resultado: $randomNumber", Toast.LENGTH_SHORT).show()
+        }
+        videodado.start()
+    }
 }
